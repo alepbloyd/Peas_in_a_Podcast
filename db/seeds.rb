bab8968a9529b5aa0524229eac0552b82ffc2443
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-06 17:04:51 -0400

    User story 1, parent index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Podcast.create!(name: "Criminal", in_production: true, ad_slot_cost: 950.25)
+Podcast.create!(name: "Maintenance Phase", in_production: true, ad_slot_cost: 500)
