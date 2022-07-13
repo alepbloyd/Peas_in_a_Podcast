0a1c6ede686b7bb240a650101aad537bc9b8d020
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-12 21:31:13 -0400

    add length_in_seconds display back in

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe Podcast, type: :model do
     it {should allow_value(false).for(:in_production) }
   end
 
+  it 'returns "created_at" in "%a, %e %b %Y, %H:%M:%S" format' do
+    podcast_1 = Podcast.create!(name: "This Canadian Death", in_production: true, ad_slot_cost: 950.25)
+
+    allow(podcast_1).to receive(:created_date_format).and_return('Tue, 12 Jul 2022, 06:46:39')
+
+    expect(podcast_1.created_date_format).to eq("Tue, 12 Jul 2022, 06:46:39")
+  end
+
+  it 'returns "updated_at" in "%a, %e %b %Y, %H:%M:%S" format' do
+    podcast_1 = Podcast.create!(name: "This Canadian Death", in_production: true, ad_slot_cost: 950.25)
+
+    allow(podcast_1).to receive(:updated_date_format).and_return('Tue, 12 Jul 2022, 06:46:39')
+
+    expect(podcast_1.updated_date_format).to eq("Tue, 12 Jul 2022, 06:46:39")
+  end
+
 end
